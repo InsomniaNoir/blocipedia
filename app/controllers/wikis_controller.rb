277277c98516a3c7c057b87b3ec459005cd74a79
Jeremiah Shafer <jrshafer@mac.com>
2015-08-25 20:13:25 -0500

    Add wiki_controller with CRUD methods

---
+++ app/controllers/wikis_controller.rb
@@ -0,0 +1,43 @@
+class WikisController < ApplicationController
+  def index
+    @wikis = Wiki.all
+  end
+
+  def show
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def new
+    @wiki = Wiki.new
+  end
+
+  def create
+    @wiki = Wiki.new( wiki_params )
+    if @wiki.save
+      flash[:notice] = "Great success!"
+      redirect_to @wiki
+    else
+      flash[:error] = "Maybe you should take up a new hobby... like knitting!"
+      redirect_to :new
+    end
+  end
+
+  def edit
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+    if @wiki.update_attributes( wiki_params )
+      flash[:notice] = "Great update success!"
+    else
+      flash[:error] = "You're not very good at this, are you?"
+    end
+  end
+
+  private
+
+  def wiki_params
+    params.require(:wiki).permit(:title, :body, :private)
+  end
+end
